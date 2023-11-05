10aa195df60ebf9edacef1a09b73c1a672b61c64
Victor Marcano <victormarkno@gmail.com>
2023-11-05 17:37:24 -0300

    agregar los detalles del producto con el CSS. Actualizacion de CSS

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 shoppingCartMenu.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     mobileMenu.classList.toggle('inactive');
@@ -37,7 +37,7 @@ function toggleCartAside () {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 
 }
 
