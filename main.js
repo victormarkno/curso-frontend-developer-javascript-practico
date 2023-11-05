f292db19d5f20953e3c1313a19339a4eb5f7905f
Victor Marcano <victormarkno@gmail.com>
2023-11-05 15:59:40 -0300

    se agregaron las funciones de cart aside para activar y desactivar

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
+const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuBurgerIcon.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartMenu.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+    
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive')
+
 }
