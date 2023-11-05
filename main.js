6534a6bbc8e0f4bba4ef8d7e7e3cb010917a7419
Victor Marcano <victormarkno@gmail.com>
2023-11-05 18:27:45 -0300

    agrega funcionalidad a abrir y cerrar producto, tambien entre menus

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
-const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
+const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 shoppingCartMenu.addEventListener('click', toggleCartAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
-function toggleDesktopMenu () {
+function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
@@ -20,25 +23,42 @@ function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu () {
+function toggleMobileMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        shoppingCartContainer.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive');
     }
     
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCartAside () {
+function toggleCartAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
+    
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
-    shoppingCartContainer.classList.toggle('inactive')
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
 
+    shoppingCartContainer.classList.toggle('inactive');
+
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -62,6 +82,16 @@ productList.push({
     price: 890,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
+productList.push({
+    name: 'Desktop',
+    price: 890,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Desktop',
+    price: 890,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
 for (product of productList) {
     const productCard = document.createElement('div');
@@ -69,6 +99,7 @@ for (product of productList) {
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
